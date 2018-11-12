502f8dce20a4e96addd9dee2e42fdd7af14c46a4
Wesley Griffin <wesley.griffin@nist.gov>
2018-11-12 10:50:58 -0600

    Replace std::pair with MyPair to simplify further.

--- ex1.cc
+++ ex1.cc
@@ -1,16 +1,20 @@
 #include "tao/json.hpp"
-#include <utility>
 
 template <typename T>
 struct MyTraits : public tao::json::traits<T> {};
 
 using MyValue = tao::json::basic_value<MyTraits>;
 
-template <typename U, typename V>
-struct MyTraits<std::pair<U, V>>
-  : public tao::json::binding::object<
-      TAO_JSON_BIND_REQUIRED("first", &std::pair<U, V>::first),
-      TAO_JSON_BIND_REQUIRED("second", &std::pair<U, V>::second)> {};
+struct MyPair {
+  bool first;
+  int second;
+};
+
+template <>
+struct MyTraits<MyPair> : public tao::json::binding::object<
+                            TAO_JSON_BIND_REQUIRED("first", &MyPair::first),
+                            TAO_JSON_BIND_REQUIRED("second", &MyPair::second)> {
+};
 
 std::string const str1 = R"({
   "first": true,
@@ -19,6 +23,5 @@ std::string const str1 = R"({
 
 int main(void) {
   MyValue const value = tao::json::basic_from_string<MyTraits>(str1);
-  auto pair = value.as<std::pair<bool, int>>();
+  auto pair = value.as<MyPair>();
 }
-
